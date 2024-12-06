8ee9f8dd2e564646fb0dc7f7e9f867fd942e45fe
Leonardo Rosendo <117026317+Leopardim26@users.noreply.github.com>
2024-12-05 23:15:35 -0300

    soma_facil.cpp

---
+++ soma_facil.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Lendo a entrada do exercício
+    int A, B;
+    cin >> A >> B;
+
+    // Seu código vai aqui
+    cout << A+B;
+
+    return 0;
+}
