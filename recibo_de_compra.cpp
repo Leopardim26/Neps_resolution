2eca1a0d0d7dacf1926f8bb84f1819223cc5ca0c
Leonardo Rosendo <117026317+Leopardim26@users.noreply.github.com>
2024-12-12 14:12:49 -0300

    recibo_de_compra.cpp

---
+++ recibo_de_compra.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[110][22][110];
+int r, k;
+
+int solve(int value, int quant, int smaller){
+    if(dp[value][quant][smaller] != -1) return dp[value][quant][smaller];
+    
+    if(value == 0){
+        if(quant == 0){   
+            return dp[value][quant][smaller] = 1;
+        }else{
+            return dp[value][quant][smaller] = 0;
+        }
+    }else{
+        if(quant == 0){
+            return dp[value][quant][smaller] = 0;
+        }
+    }
+
+    dp[value][quant][smaller] = 0;
+
+    for(int i=1; i<min(value+1, smaller); i++){
+        dp[value][quant][smaller] += solve(value-i, quant-1, i);
+    }
+
+    return dp[value][quant][smaller];
+}
+
+int main(){
+    cin >> r >> k;
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << solve(r, k, r+1) << "\n";
+
+    return 0;
+}
